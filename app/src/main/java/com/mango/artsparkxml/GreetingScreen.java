a8e0a7eafcab6dc702d0cae046c9936228fa1ab7
mangosidekick <pokeyp0kers3@gmail.com>
2024-05-21 18:44:29 +0800

    oldver

--- app/src/main/java/com/mango/artsparkxml/GreetingScreen.java
+++ app/src/main/java/com/mango/artsparkxml/GreetingScreen.java
@@ -1,9 +1,12 @@
 package com.mango.artsparkxml;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +15,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class GreetingScreen extends AppCompatActivity implements View.OnClickListener {
+    private android.widget.EditText EditText;
+    EditText userName = (EditText);
+    private android.widget.Button Button;
+    Button enterBtn = (Button);
+    private android.widget.TextView TextView;
+    TextView txt = (TextView);
 
-    private Button BtnEnter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +30,10 @@ public class GreetingScreen extends AppCompatActivity implements View.OnClickLis
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_greeting_screen);
 
-        BtnEnter = findViewById(R.id.enterbutton);
-        BtnEnter.setOnClickListener(this);
+       enterBtn = findViewById(R.id.enterbutton);
+       enterBtn.setOnClickListener(this);
+        txt = findViewById(R.id.username);
+        txt.setText(userName.getText().toString());
     }
 
     public void onClick(View v) {
--- app/src/main/java/com/mango/artsparkxml/WelcomeScreen.java
+++ app/src/main/java/com/mango/artsparkxml/WelcomeScreen.java
@@ -3,7 +3,9 @@ package com.mango.artsparkxml;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -18,11 +20,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class WelcomeScreen extends AppCompatActivity implements View.OnClickListener {
 
-    private android.widget.Button Button;
-    Button enterBtn = (Button);
     private android.widget.EditText EditText;
+    private android.database.sqlite.SQLiteDatabase SQLiteDatabase;
+    private android.widget.Button Button;
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
+    Button enterBtn = (Button);
     EditText userName = (EditText);
+    SQLiteDatabase db = (SQLiteDatabase);
     String prevStarted = "yes";
+
 
     @Override
     protected void onResume() {
@@ -43,8 +50,13 @@ public class WelcomeScreen extends AppCompatActivity implements View.OnClickList
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome_screen);
 
-        userName = findViewById(R.id.entername);
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
 
+        mEditor.putString("key", "mitch");
+        mEditor.commit();
+
+        userName = findViewById(R.id.entername);
         enterBtn = findViewById(R.id.enterbutton);
         enterBtn.setOnClickListener(this);
     }
@@ -57,30 +69,18 @@ public class WelcomeScreen extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-
         if( TextUtils.isEmpty(userName.getText()) && v.getId() == R.id.enterbutton ){
             Toast.makeText(this, "Name is required!", Toast.LENGTH_SHORT).show();
-            userName.setError( "First name is required!" );
-
         }else{
-            startActivity(new Intent(WelcomeScreen.this, MoodboardMenu.class));
-            Intent i = new Intent(getApplicationContext(), WelcomeScreen.class);
-            startActivity(i);
+            String name = userName.getText().toString();
+            mEditor.putString(getString(R.string.username), name);
+            mEditor.commit();
+            Intent intent = new Intent(WelcomeScreen.this, MoodboardMenu.class);
+            startActivity(intent);
         }
 
+
     }
-        /*if (v.getId() == R.id.enterbutton) {
-            startActivity(new Intent(WelcomeScreen.this, MoodboardMenu.class));
-
-            if( TextUtils.isEmpty(userName.getText())){
-                Toast.makeText(this, "Name is required!", Toast.LENGTH_SHORT).show();
-                userName.setError( "First name is required!" );
-
-            }else{
-                Intent i = new Intent(getApplicationContext(), WelcomeScreen.class);
-                startActivity(i);
-            }
-        }*/
 
 
 }
