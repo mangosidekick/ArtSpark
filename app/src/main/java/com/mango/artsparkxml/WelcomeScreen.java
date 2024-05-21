76c9ff0331b68d72cd4cc7d7ea6ecae22a8de8b6
Fayya Anyatasia <anyatasia@gmail.com>
2024-05-21 12:49:43 +0700

    make welcome screen to be the first and setup and save to shared preferences

--- app/src/main/java/com/mango/artsparkxml/WelcomeScreen.java
+++ app/src/main/java/com/mango/artsparkxml/WelcomeScreen.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,41 +11,44 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class WelcomeScreen extends AppCompatActivity implements View.OnClickListener {
+public class WelcomeScreen extends AppCompatActivity {
+    private Button enterBtn;
+    private EditText enterName;
 
-    private android.widget.Button Button;
-    Button enterBtn = (Button);
-    private android.widget.EditText EditText;
-    EditText userName = (EditText);
-    String prevStarted = "yes";
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        SharedPreferences sharedpreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
-        if (!sharedpreferences.getBoolean(prevStarted, false)) {
-            SharedPreferences.Editor editor = sharedpreferences.edit();
-            editor.putBoolean(prevStarted, Boolean.TRUE);
-            editor.apply();
-        } else {
-            moveToSecondary();
-        }
-    }
+    private final String SHARED_PREF_NAME = getString(R.string.app_name);
+    private final String FIRST_START_KEY = "firstStart";
+    private final String USERNAME_KEY = "username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_welcome_screen);
 
-        userName = findViewById(R.id.entername);
+        SharedPreferences sharedpreferences = getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
 
-        enterBtn = findViewById(R.id.enterbutton);
-        enterBtn.setOnClickListener(this);
+        // if this is the first time running, run the enter username screen
+        if (sharedpreferences.getBoolean(FIRST_START_KEY, true)) {
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putBoolean(FIRST_START_KEY, false);
+            editor.commit();
+
+            // add setup welcome screen
+            setContentView(R.layout.activity_welcome_screen);
+
+            enterName = findViewById(R.id.entername);
+
+            enterBtn = findViewById(R.id.enterbutton);
+            enterBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        saveName();
+                    }
+                }
+            );
+        } else {
+            moveToSecondary();
+        }
     }
 
     public void moveToSecondary(){
@@ -55,32 +57,21 @@ public class WelcomeScreen extends AppCompatActivity implements View.OnClickList
         startActivity(intent);
     }
 
-    @Override
-    public void onClick(View v) {
+    private void saveName() {
+        String name = enterName.getText().toString();
 
-        if( TextUtils.isEmpty(userName.getText()) && v.getId() == R.id.enterbutton ){
-            Toast.makeText(this, "Name is required!", Toast.LENGTH_SHORT).show();
-            userName.setError( "First name is required!" );
-
-        }else{
-            startActivity(new Intent(WelcomeScreen.this, MoodboardMenu.class));
-            Intent i = new Intent(getApplicationContext(), WelcomeScreen.class);
-            startActivity(i);
+        if (name.isEmpty()) {
+            Toast.makeText(WelcomeScreen.this, "Name is required!", Toast.LENGTH_SHORT).show();
+            enterName.setError( "First name is required!" );
+            return;
         }
 
+        // if username available, save it into shared preferences
+        SharedPreferences sp = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putString(USERNAME_KEY, name);
+        editor.apply();
     }
-        /*if (v.getId() == R.id.enterbutton) {
-            startActivity(new Intent(WelcomeScreen.this, MoodboardMenu.class));
-
-            if( TextUtils.isEmpty(userName.getText())){
-                Toast.makeText(this, "Name is required!", Toast.LENGTH_SHORT).show();
-                userName.setError( "First name is required!" );
-
-            }else{
-                Intent i = new Intent(getApplicationContext(), WelcomeScreen.class);
-                startActivity(i);
-            }
-        }*/
-
 
 }
