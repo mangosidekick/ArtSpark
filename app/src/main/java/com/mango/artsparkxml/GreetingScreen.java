53470b485538df16c58fb98ebeefb526fbcdb99a
mangosidekick <pokeyp0kers3@gmail.com>
2024-05-22 19:39:49 +0800

    update

--- app/src/main/java/com/mango/artsparkxml/GreetingScreen.java
+++ app/src/main/java/com/mango/artsparkxml/GreetingScreen.java
@@ -30,8 +30,10 @@ public class GreetingScreen extends AppCompatActivity implements View.OnClickLis
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_greeting_screen);
 
-        Random random = new Random();
-        int randomNumber = random.nextInt(16);
+        int[] strs = new int[] {R.string.quote1, R.string.quote2, R.string.quote3, R.string.quote4, R.string.quote5, R.string.quote6, R.string.quote7, R.string.quote8, R.string.quote9, R.string.quote10, R.string.quote11, R.string.quote12, R.string.quote13, R.string.quote14, R.string.quote15};
+        int randomIndex = new Random().nextInt(15);
+        int resId = strs[randomIndex];
+        String randomString = getString(resId);
 
         // set user as the saved username
         SharedPreferences sharedpreferences = getApplicationContext().getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
@@ -41,7 +43,7 @@ public class GreetingScreen extends AppCompatActivity implements View.OnClickLis
         userText.setText(name);
 
         quote = findViewById(R.id.quote);
-        quote.setText();
+        quote.setText(randomString);
 
         BtnEnter = findViewById(R.id.enterbutton);
         BtnEnter.setOnClickListener(this);
