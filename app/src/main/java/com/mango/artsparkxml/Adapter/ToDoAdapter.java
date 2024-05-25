e0f2f58d14328be91562cdc363f29fcc6a6fa1b6
mangosidekick <pokeyp0kers3@gmail.com>
2024-05-25 16:17:00 +0800

    task list setup

---
+++ app/src/main/java/com/mango/artsparkxml/Adapter/ToDoAdapter.java
@@ -0,0 +1,57 @@
+package com.mango.artsparkxml.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mango.artsparkxml.CalendarTodolist;
+import com.mango.artsparkxml.Model.ToDoModel;
+import com.mango.artsparkxml.R;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+
+    private List<ToDoModel> todoList;
+    private CalendarTodolist activity;
+
+    public ToDoAdapter(CalendarTodolist activity) {
+        this.activity = activity;
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.task_layout, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    public void onBindViewHolder(ViewHolder holder, int position){
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+
+    public int getItemCount(){
+        return todoList.size();
+    }
+
+    private boolean toBoolean(int n){
+        return n!=0;
+    }
+
+    public void setTasks(List<ToDoModel> todoList){
+        this.todoList = todoList;
+        notifyDataSetChanged();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+
+        ViewHolder(View view){
+            super(view);
+            task = view.findViewById(R.id.todoCheckBox);
+        }
+    }
+}
--- app/src/main/java/com/mango/artsparkxml/CalendarTodolist.java
+++ app/src/main/java/com/mango/artsparkxml/CalendarTodolist.java
@@ -10,9 +10,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mango.artsparkxml.Adapter.ToDoAdapter;
+import com.mango.artsparkxml.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CalendarTodolist extends AppCompatActivity implements View.OnClickListener {
 
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+
+    private List<ToDoModel> taskList;
     ImageButton artSparkBurgerIcon, artSparkMoodBoardIcon, artSparkCalendarIcon, artSparkYouIcon;
 
     @Override
@@ -20,7 +32,27 @@ public class CalendarTodolist extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calendar_todolist);
+        getSupportActionBar().hide();
 
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("this is a test task");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);
     }
 
     @Override
